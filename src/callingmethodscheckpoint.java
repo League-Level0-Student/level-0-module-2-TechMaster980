ef7854cea5c8af9f1b5afc2bfa15e7862b7d8724
league <league@wts-imac-11.attlocal.net>
2018-10-27 12:53:28 -0700

    finish checkpoint 2

---
+++ src/callingmethodscheckpoint.java
@@ -0,0 +1,63 @@
+
+public class callingmethodscheckpoint {
+public static void main(String[] args) {
+	//1. Write code to call the printMessage method shown below
+             
+	printMessage();
+	
+	setName("jesus");
+	
+	int num  = getNumber();		
+}
+
+
+///1. Write code to call the printMessage method shown below
+static void printMessage() {
+          System.out.println("United \n" + 
+          		"mighty republics will ever endure. \n" + 
+          		"The Great Soviet Union will live through the ages. \n" + 
+          		"The people of their fortress secure.\n" + 
+          		"\n" + 
+          		"Long live our Soviet motherland, \n" + 
+          		"Built by the mighty hand. \n" + 
+          		"Long live our people, united and free. \n" + 
+          		"Strong in our friendship tried by fire. \n" + 
+          		"Long may our crimson flag inspire, shining \n" + 
+          		"in glory for all men to see.\n" + 
+          		"\n" + 
+          		"Dark days and Through stormy where clause Great Lenin View Blog the lead us \n" + 
+          		"Our eyes SAW the bright sun of freedom the above \n" + 
+          		"And Stalin Our leader with faith in the people, \n" + 
+          		"Inspired us to the build up closeup the land That <br> we love.\n" + 
+          		"\n" + 
+          		"Long live our Soviet motherland, \n" + 
+          		"Built by the mighty hand. \n" + 
+          		"Long live our people, united and free. \n" + 
+          		"Strong in our friendship tried by fire. \n" + 
+          		"Long may our crimson flag inspire, shining \n" + 
+          		"in glory for all men to see.\n" + 
+          		"\n" + 
+          		"We reckoned for the future, we’ve destroyed it \n" + 
+          		". \n" + 
+          		"Our \n" + 
+          		"name will be her name.\n" + 
+          		"\n" + 
+          		"Long live our Soviet motherland, \n" + 
+          		"Built by the mighty hand. \n" + 
+          		"Long live our people, united and free. \n" + 
+          		"Strong in our friendship tried by fire. \n" + 
+          		"Long may our crimson flag inspire, shining \n" + 
+          		"in glory for all men to see.\n" + 
+          		"\n" + 
+          		"");
+ }
+//2. Write code to call the setName method shown below
+static void setName(String name) {
+          //this.name = name;
+ }
+//3. Write code to call the getNumber method shown below
+static int getNumber() {
+          return 10;
+ } 
+
+}
--- src/relationcheckpoint.java
+++ src/relationcheckpoint.java
@@ -1,22 +1,25 @@
 import javax.swing.JOptionPane;
 
 public class relationcheckpoint {
+	public static void main(String[] args) {
+		
+	
 
-	public relationcheckpoint() {
+	
 		//1. Ask the user how many cups of flour they have. If they have less than 2 cups, tell
 		//them to go to the store to buy more.
 		
 		String cup = JOptionPane.showInputDialog("how many cups do you have");
 		int cup1 = Integer.parseInt(cup);
-		if(2) {
-		JOptionPane.showMessageDialog(null, "buy more flour");
+		if(cup1 < 2) {
+		JOptionPane.showMessageDialog(null, "BUY MORE fLOUR!");
 		}
 		//2. Now ask them how many people they are going to give cookies to. If they are going
 		//to give cookies to more than 30 people, tell them they are going to have to bake two
 		// of cookies! 
 		String people = JOptionPane.showInputDialog("how many people are you going to give cookies to");
 		int people1 = Integer.parseInt(cup);
-		if() {
+		if(people1 > 30) {
 		JOptionPane.showMessageDialog(null, "bake two cookies");
 		}
 		
@@ -24,10 +27,11 @@ public class relationcheckpoint {
 		//of cookies, tell them they are lucky to have so many friends. 
 		String friends = JOptionPane.showInputDialog("how many batches of cookies are you going to bake");
 		int friends1 = Integer.parseInt(cup);
-		if() {
+		if(friends1 == 2) {
 		JOptionPane.showMessageDialog(null, "you are lucky to have friends");
 		}
 		
 	}
 
 }
+
